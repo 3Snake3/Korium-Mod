039db384a4fee64642b864cdad7d175426e5918f
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-08-06 00:12:41 +0300

    Snapshot 0.03c

--- scripts/sunlinght/blocks/power.js
+++ scripts/sunlinght/blocks/power.js
@@ -1,4 +1,5 @@
 const norium = Vars.content.getByName(ContentType.item, "koriummod-norium");
+const korium = Vars.content.getByName(ContentType.item, "koriummod-korium");
 
 //NORIUM SOLAR PANEL
 
@@ -17,3 +18,25 @@ const noriumPanel = extendContent(SolarGenerator, "norium-solar-panel", {
 });
 
 noriumPanel.requirements = ItemStack.with(norium, 140, Items.metaglass, 75);
+
+//KORIUM POWER NODE
+
+const koriumPowerNode = extendContent(PowerNode, "korium-power-node", {
+	
+	localizedName: "Korium power node",
+	description: "With the help of Korium, it transmits energy over long distances and a large number of points",
+	
+	size: 2,
+	health: 300,
+	maxNodes: 48,
+	laserRange: 26.8,
+	
+	category: Category.power,
+	buildVisibility: BuildVisibility.shown,
+	
+	laserColor2: Color.valueOf("#676386"),
+        laserColor1: Color.valueOf("#8276a6"),
+	
+});
+
+koriumPowerNode.requirements = ItemStack.with(korium, 42);
