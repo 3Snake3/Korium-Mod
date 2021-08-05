ee6dbe2e96f261e7ea76e31c7ca48f2988edecdd
SuperJaba2000 <73773387+SuperJaba2000@users.noreply.github.com>
2021-08-06 00:10:27 +0300

    Update power.js

--- scripts/sunlinght/power.js
+++ scripts/sunlinght/power.js
@@ -1,19 +1,1 @@
-const koriumPowerNode = extendContent(PowerNode, "korium-power-node", {
-	
-	localizedName: "Korium power node",
-	description: "With the help of Korium, it transmits energy over long distances and a large number of points",
-	
-	size: 2,
-	health: 300,
-	maxNodes: 48,
-	laserRange: 26.8,
-	
-	category: Category.power,
-	buildVisibility: BuildVisibility.shown,
-	
-	laserColor1: Color.valueOf("#676386"),
-        laserColor2: Color.valueOf("#8276a6"),
-	
-});
 
-koriumPowerNode.requirements = ItemStack.with(Vars.content.getByName(ContentType.item, "koriummod-korium"), 42);
