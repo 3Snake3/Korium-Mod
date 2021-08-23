19b12457deb2d72c4b7a5e1ed94af3186b3e6eda
SuperJaba2000 <superjaba2000@gmail.com>
2021-08-23 07:53:08 +0300

    Update defense.js

--- scripts/sunlinght/blocks/defense.js
+++ scripts/sunlinght/blocks/defense.js
@@ -26,11 +26,65 @@ function setLightning(obj){
 	obj.lightningColor = korium.color;
 };
 
-const koriumWall = newWall("korium-wall", "Korium wall", 410, 1);
-    koriumWall.requirements = ItemStack.with(korium, 6);
-    setLightning(koriumWall);
+const koriumWall = extendContent(Wall, "korium-wall", {
 	
+	localizedName: "Korium wall",
+	description: "--",
+	
+	buildVisibility: BuildVisibility.shown,
+	category: Category.defense,
+		
+	size: 1,
+	health: 410,
+	
+    koriumWall.requirements = ItemStack.with(korium, 6);
+	
+
+});
+
+korium.buildType = () => {
+	const ent = extendContent(Wall.WallBuild, koriumWall, {
+    
+	    charge: 0,
+		
+		init(){
+            this.getBARS();
+            
+            this.maxHealth = this.koriumWall().maxHealth;
+            //this.health = this.koriumWall().consHealth;
+            
+            return this.super$init();
+        },
+	
+	    display(table){
+			table.table(cons(bars => {
+                bars.defaults().growX().height(18).pad(4);
+
+                this.BARS.each(bar => {
+                    table.add(bar).growX();
+                    table.row();
+                });
+
+            })).growX();
+            table.row();
+		},
+		
+		getBARS(){
+			this.BARS = new Seq();
+            
+            this.BARS.add(new Bar(Core.bundle.get("stat.health"), Pal.lancerLaser, floatp(() => this.healthcf())));
+            this.BARS.add(new Bar(Core.bundle.get("stat.charge"), Pal.accent, floatp(() => this.phasef())));
+		},
+	});
+	return ent;
+};
+
+setLightning(koriumWall);
+	
+//////////////////////////////////////////////////////////////////////////////////
+	
+/*
 const koriumWallLarge = newWall("korium-wall-large", "Large Korium wall", 1640, 2);
     koriumWallLarge.requirements = ItemStack.with(korium, 24);
 	setLightning(koriumWallLarge);
-
+*/
