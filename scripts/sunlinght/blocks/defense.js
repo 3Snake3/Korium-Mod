609bfe5a99bb49bf3280a23877230b223510e5d2
SuperJaba2000 <superjaba2000@gmail.com>
2021-08-23 21:45:05 +0300

    Update defense.js

--- scripts/sunlinght/blocks/defense.js
+++ scripts/sunlinght/blocks/defense.js
@@ -1,26 +1,65 @@
 const korium = Vars.content.getByName(ContentType.item, "koriummod-korium");
 
-class chargingWall extends Wall{
+function ChargingWall(name, maxChargeStages, requiredCharge, stageUpgradeMultiplier, additionally){
 	
-	chargeStages = 1;
+	this = extendContent(Wall, name, additionally);
+	block = this;
 	
-	constructor(name, chargeStages){
-		super(name);
-		
-		this.chargeStages = chargeStages;
+	this.buildType = () => {
+		return extendContent(Wall.WallBuild, block, {
+			
+			charge: 0,
+			chargeStage: 1,
+			maxChargeStages: maxChargeStages,
+			
+			upgradeMultiplier: stageUpgradeMultiplier,
+			requiredCharge: requiredCharge,
+			
+			damage(damage){
+				
+				this.charge += damage / 10;
+				
+				checkStages();
+				
+                print("Special damage (" + damage + "), ");
+		        this.super$damage(damage);
+            },
+			
+			checkStages(){
+				
+				if(this.chargeStage >= this.maxChargeStage){
+					this.chargeStage = this.maxChargeStage;
+				}else{
+					
+				    if(this.charge >= (this.requiredCharge * this.chargeStage)){
+						
+						this.charge = 0;
+						
+						if(!(this.chargeStage >= this.maxChargeStage)){
+							this.chargeStage++;
+						    print("Update Charge Stage to" + this.chargeStage);
+						}
+					}
+				}
+			},
+			
+		    onDestroyed(){
+			    this.super$onDestroyed();
+				
+				Sounds.spark.at(this.tile);
+
+                Damage.damage(this.x, this.y, this.upgradeMultiplier * this.chargeStage * tilesize, 10 * (this.upgradeMultiplier * this.chargeStage) * 4);
+
+                //Effect.shake(2, 5, this.x, this.y);
+                //explodeEffect.at(x, y);
+				
+                print("EXPLOSION OF THE " + this.chargeStage + " CHARGE STAGE WALL, ");
+			},
+		});
 	}
 }
-function setLightning(obj){
-    obj.insulated = true;
-    obj.absorbLasers = true;
-    obj.schematicPriority = 10;
-    obj.lightningChance = 0.42;
-    obj.lightningDamage = 38;
-	obj.lightningLength = 24;
-	obj.lightningColor = korium.color;
-};
 
-const koriumWall = extendContent(Wall, "korium-wall", {
+const koriumWall = new ChargingWall("korium-wall", 5, 10, 1.3, {
 	
 	localizedName: "Korium wall",
 	description: "--",
@@ -30,97 +69,11 @@ const koriumWall = extendContent(Wall, "korium-wall", {
 		
 	size: 1,
 	health: 410,
-	configurable: true,
 	
     requirements: ItemStack.with(korium, 6),
 });
 
-koriumWall.buildType = () => {
-	const ent = extendContent(Wall.WallBuild, koriumWall, {
-    
-	    charge: 1,
-		
-		init(){
-            this.getBARS();
-            
-            this.maxHealth = 410;
-            //this.health = koriumWall.health;
-            
-            //return this.super$init();
-        },
-	
-	    display(table){
-			
-			    this.super$display(table);
-			
-			    getBARS();
-			
-                table.table(cons(t => {
-                    t.left();
-                    t.add(new Image(this.block.getDisplayIcon(this.tile))).size(8 * 4);
-                    t.labelWrap(this.block.getDisplayName(this.tile)).left().width(190).padLeft(5);
-                })).growX().left();
-                table.row();
-                
-                table.table(cons(bars => {
-                    bars.defaults().growX().height(18).pad(4);
-                    this.displayCbars(bars);
-                })).growX();
-                table.row();
-                
-                let charge = this.charge;
-                let chargePhase = this.charge;//TODO
-                
-                table.table(cons(t => {
-                    const rebuild = new RunnableAction();
-                    rebuild.setRunnable(() => {
-                        t.clearChildren();
-                        t.left();
-                        
-                        t.add(Core.bundle.format("korium-wall-charge", charge)).left();
-                        t.row();
-                        
-//                        t.table(cons(t2 => {
-//                            t2.left();
-//                                
-//                            t2.add(new Image(itemf.item.icon(Cicon.small)));
-//                            t2.labelWrap(amount + " / " + itemf.amount).left().width(190).padLeft(5);
-//                        })).left();
-                    });
-                    
-                rebuild.run();
-//                t.update(() => {
-//                        amount = this.items != null ? this.items.get(itemf.item) : 0;
-//                        itemf = this.phaseReq(this.phase);
-//                        phase = this.phase;
-//                            
-//                        rebuild.run();
-//                });
-            })).padTop(12).growX().left();
-                
-            table.marginBottom(-5);
-        },
-		
-		getBARS(){
-			this.cbars = new Seq();
-            
-            this.cbars.add(new Bar(Core.bundle.get("stat.health"), Pal.lancerLaser, floatp(() => this.healthcf())));
-            this.cbars.add(new Bar(Core.bundle.get("stat.charge"), Pal.accent, floatp(() => this.charge)));
-		},
-		
-		healthcf(){
-            return this.health / this.maxHealth;
-        },
-		
-		displayCbars(table){
-            this.cbars.each(bar => {
-                table.add(bar).growX();
-                table.row();
-            });
-        },
-	});
-	return ent;
-};
+
 
 //setLightning(koriumWall);
 	
@@ -131,5 +84,9 @@ const koriumWallLarge = newWall("korium-wall-large", "Large Korium wall", 1640, 
     koriumWallLarge.requirements = ItemStack.with(korium, 24);
 	setLightning(koriumWallLarge);
 */
+
+
+
+
 
 
