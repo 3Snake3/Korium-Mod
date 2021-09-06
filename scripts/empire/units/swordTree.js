7bd9cf0887a2db11362c5a143bc2481ff4434236
SuperJaba2000 <superjaba2000@gmail.com>
2021-09-06 22:05:10 +0300

    Update swordTree.js

--- scripts/empire/units/swordTree.js
+++ scripts/empire/units/swordTree.js
@@ -1,18 +1,5 @@
 const getTex = tex => Core.atlas.find("koriummod-" + tex);
 
-//weapons
-
-/*const swordWeapon = extend(Weapon, "sword-weapon", {
-    reload: 10,
-    x: 4,
-    y: 2,
-    top: false,
-    ejectEffect: Fx.casing1,
-    bullet: Bullets.standardCopper,
-});*/
-
-//units
-
 const sword = extendContent(UnitType, "sword", {
 	
     localizedName: "Sword",
@@ -25,12 +12,18 @@ const sword = extendContent(UnitType, "sword", {
 	init(){
         this.weapons.add(
 		    extend(Weapon, "sword-weapon", {
-            reload: 10,
-            x: 4,
-            y: 2,
-            top: false,
-            ejectEffect: Fx.casing1,
-            bullet: Bullets.standardCopper,
+				load(){
+					this.region = getTex(this.name);
+					this.outlineRegion = getTex(this.name + "-outline");
+					/*this.heatRegion =*/
+				},
+				
+                reload: 10,
+                x: 4,
+                y: 2,
+                top: false,
+                ejectEffect: Fx.casing1,
+                bullet: Bullets.standardCopper,
             })
 		)
 		this.super$init();
