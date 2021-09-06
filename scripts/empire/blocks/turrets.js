0b5c98158b5350a02a6807dc88d7e1ba47518cac
SuperJaba2000 <superjaba2000@gmail.com>
2021-09-06 09:03:24 +0300

    ..

--- scripts/empire/blocks/turrets.js
+++ scripts/empire/blocks/turrets.js
@@ -2,40 +2,83 @@ const korium = Vars.content.getByName(ContentType.item, "koriummod-korium");
 const siron = Vars.content.getByName(ContentType.item, "koriummod-siron");
 const kapronite = Vars.content.getByName(ContentType.item, "koriummod-kapronite");
 
-const fragKorium = extendContent(FlakBulletType, 4.5, 15, {
-            ammoMultiplier: 4,
-            splashDamage: 54 * 1.5,
-            splashDamageRadius: 42,
-            lightning: 4,
-            lightningLength: 11,
-			lightningColor: korium.color,
-            shootEffect: Fx.shootBig,
-            collidesGround: true,
-            explodeRange: 20,
+//bullets
+
+const fragKorium = extendContent(FlakBulletType, 4.5, 14, {
+    ammoMultiplier: 4,
+    splashDamage: 54 * 1.5,
+    splashDamageRadius: 42,
+    lightning: 4,
+    lightningLength: 11,
+	lightningColor: korium.color,
+    shootEffect: Fx.shootBig,
+    explodeRange: 20,
 });
 
-const fragSiron = extendContent(FlakBulletType, 3, 19, {
-            splashDamageRadius: 30,
-            splashDamage: 20 * 1.5,
+const fragSiron = extendContent(FlakBulletType, 3.8, 19, {
+    splashDamageRadius: 30,
+    splashDamage: 20 * 1.5,
 			
-            fragBullet: extendContent(BasicBulletType, 2, 15, "bullet", {
-                width: 11,
-                height: 12,
-                shrinkY: 1,
-                lifetime: 15,
-                frontColor: Color.valueOf("#862424"),
-                backColor: Color.valueOf("#6c1717"),
-                despawnEffect: Fx.none,
-            }),
+    fragBullet: extendContent(BasicBulletType, 2.4, 11, "bullet", {
+        width: 11,
+        height: 12,
+        shrinkY: 1,
+        lifetime: 19,
+        frontColor: Color.valueOf("#862424"),
+        backColor: Color.valueOf("#6c1717"),
+        despawnEffect: Fx.none,
+    }),
 			
-            fragBullets: 5,
-            hitEffect: Fx.plasticExplosion,
-            frontColor: Color.valueOf("#862424"),
-            backColor: Color.valueOf("#6c1717"),
-            shootEffect: Fx.shootBig,
-            collidesGround: true,
-            explodeRange: 20,
+    fragBullets: 5,
+    hitEffect: Fx.plasticExplosion,
+    frontColor: Color.valueOf("#862424"),
+    backColor: Color.valueOf("#6c1717"),
+    shootEffect: Fx.shootBig,
+    collidesGround: true,
+    explodeRange: 22,
 });
+
+const bigArtillery = extendContent(ArtilleryBulletType, 4.5, 33.7, {
+	lifeTime: 149,
+	splashDamageRadius: 32,
+	splashDamage: 18 * 1.9,
+	fragLifeMin: 0.7,
+	lightRadius: 47,
+    lightColor: Items.pyratite.color,
+    lightOpacity: 0.72,
+	width: 29.6,
+	height: 29.2,
+	
+	fragBullet: extendContent(FlakBulletType, 3.4, 15, {
+		lifeTime: 64,
+		splashDamageRadius: 23,
+		splashDamage: 11 * 1.2,
+		fragLifeMin: 0.4,
+		lightRadius: 28,
+        lightColor: Items.pyratite.color,
+        lightOpacity: 0.49,
+		width: 18.9,
+		height: 19,
+		
+		fragBullet: extendContent(BasicBulletType, 2.9, 7, {
+			width: 11,
+            height: 12,
+            shrinkY: 1,
+            lifetime: 19,
+			lightRadius: 11,
+            lightColor: Items.pyratite.color,
+            lightOpacity: 0.2,
+            frontColor: Items.pyratite.color,
+            despawnEffect: Fx.none,
+		}),
+		
+		fragBullets: 5,
+	}),
+	
+	fragBullets: 15,
+});
+
+//turrets
 
 const vortex = extendContent(ItemTurret, "vortex", {
 	
@@ -45,23 +88,51 @@ const vortex = extendContent(ItemTurret, "vortex", {
     category: Category.turret,
     buildVisibility: BuildVisibility.shown,
 
+    size: 3,
+	health: 1755,
+
     xRand: 5,
     reloadTime: 6,
     range: 225,
-    size: 3,
     recoilAmount: 3,
     rotateSpeed: 10,
     inaccuracy: 11,
     shootCone: 35,
     shootSound: Sounds.shootSnap,
-
-    health: 1755,
 });
 
 vortex.requirements = ItemStack.with(kapronite, 300, Items.titanium, 155, siron, 95);
-
 vortex.ammo(
-                korium, fragKorium,
-                Items.plastanium, Bullets.fragPlastic,
-                siron, fragSiron
+    korium, fragKorium,
+    Items.plastanium, Bullets.fragPlastic,
+    siron, fragSiron
+);
+
+const burningSky = extendContent(ItemTurret, "burning-sky", {
+	
+	localizedName: "The Burning sky",
+    description: "BOOM",
+	
+    category: Category.turret,
+    buildVisibility: BuildVisibility.shown,
+	
+	size: 4,
+	health: 2950,
+	
+	range: 
+	minRange: 110,
+	shots: 7,
+	inaccuracy: 19.8,
+	reloadTime: 142,
+	cooldown: 0.06,
+	velocityInaccuracy: 0.4,
+	recoilAmount: 9,
+	targetAir: false,
+	shootSound: Sounds.artillery,
+	xRand: 19,
+});
+
+burningSky.requirements = ItemStack.with(kapronite, 850, siron, 540, Items.silicon, 350, Items.metaglass, 120);
+burningSky.ammo(
+    Items.pyratite, bigArtillery
 );
--- scripts/items.js
+++ scripts/items.js
@@ -18,5 +18,5 @@ const korium = newItem("korium", "Korium", 3, 0, 0.6, 0, "8276a6");
 const omnidium = newItem("omnidium", "Omnidium", 0, 0, 0, 0, "f9cea2");//change color
 const norium = newItem("norium", "Norium", 0, 0, 0.3, 0, "a21a90");//may be deleted
 const siron = newItem("siron", "Siron", 6, 0, 7.4, 0, "6c1717");
-const procionit = newItem("frozen-procionit", "Frozen Procionit", 4, 0, 0, 0, "686a9d");
+const procionit = newItem("frozen-procionite", "Frozen Procionit", 4, 0, 0, 0, "686a9d");
 const kapronite = newItem("kapronite", "Kapronite", 3, 0, 0, 0, "656469");
